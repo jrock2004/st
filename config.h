b5343b9a3cc53793b4297e29e49a06270b54f6d2
John Costanzo <jrock2004@gmail.com>
2022-06-26 14:47:53 -0400

    chore: updating font size

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Comic Code Ligatures:pixelsize=16:antialias=true:autohint=true";
+static char *font = "Comic Code Ligatures:pixelsize=18:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
